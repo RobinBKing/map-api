1d67142da441697f537d1740e90154b1933c39f7
Lindsey Raffa and Rob King <lindsey@>
2016-03-16 16:19:40 -0700

    add button and coffee near you

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,22 +1,12 @@
-  // var map;
-  // function initMap() {
-  //   map = new google.maps.Map(document.getElementById('map'), {
-  //     center: {lat: -34.397, lng: 150.644},
-  //     zoom: 8
-  //   });
-  // };
-  // 
+var initMap = require('./../js/map.js').initMap;
+var callback = require('./../js/map.js').callback;
+var createMarker = require('./../js/map.js').createMarker;
   // var apiKey = "AIzaSyAtiJ713jUWoxp9UQWe1C1o5sTTxeO-jdc";
   //
-  // $(document).ready(function() {
-  //   $('#map') = initMap();
-  //
-  //   $('#weatherLocation').click(function() {
-  //     $.get('https://maps.googleapis.com/maps/api/js?key=AIzaSyAtiJ713jUWoxp9UQWe1C1o5sTTxeO-jdc&callback=initMap').then(function(response) {
-  //       console.log(response);
-  //     });
-  //   });
-  // });
-
-
-/* Front-end User interface (jquery) code goes in this file */
+  $( document ).ready(function() {
+    $('#locateUser').click(function(){
+      initMap();
+      callback();
+      createMarker();
+    });
+  });
--- js/map.js
+++ js/map.js
@@ -1,58 +1,44 @@
-// var apiKey = "AIzaSyAtiJ713jUWoxp9UQWe1C1o5sTTxeO-jdc";
-//
-// function initMap() {
-//   var chicago = {lat: 41.85, lng: -87.65};
-//   var indianapolis = {lat: 39.79, lng: -86.14};
-//
-//   var map = new google.maps.Map(document.getElementById('map'), {
-//     center: chicago,
-//     scrollwheel: false,
-//     zoom: 7
-//   });
-//
-//   var directionsDisplay = new google.maps.DirectionsRenderer({
-//     map: map
-//   });
-//
-//   // Set destination, origin and travel mode.
-//   var request = {
-//     destination: indianapolis,
-//     origin: chicago,
-//     travelMode: google.maps.TravelMode.DRIVING
-//   };
-//
-//   // Pass the directions request to the directions service.
-//   var directionsService = new google.maps.DirectionsService();
-//   directionsService.route(request, function(response, status) {
-//     if (status == google.maps.DirectionsStatus.OK) {
-//       // Display the route on the map.
-//       directionsDisplay.setDirections(response);
-//     }
-//   });
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
 var map;
-function initMap() {
-  // navigator.geolocation.getCurrentPosition(success, error, options);
+var infowindow;
+
+exports.initMap = function () {
   navigator.geolocation.getCurrentPosition(function(location){
-  console.log(location);
+    var pyrmont = {lat: location.coords.latitude, lng: location.coords.longitude};
 
     map = new google.maps.Map(document.getElementById('map'), {
-      // center: {lat: -34.397, lng: 150.644},
-      center: {lat: location.coords.latitude, lng: location.coords.longitude},
-      zoom: 8
+      center: pyrmont,
+      zoom: 15
     });
+
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+      location: pyrmont,
+      radius: 500,
+      type: ['cafe'],
+      keyword: ['coffee']
+    }, callback);
+  });
+};
+
+exports.callback = function (results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    };
+  };
+  console.log(results);
+};
+
+exports.createMarker = function (place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
   });
 };
