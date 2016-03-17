1c40f5139efc8da1ea12ed64648f6f07e02f27bf
Lindsey Raffa and Rob King <rob@>
2016-03-17 14:17:26 -0700

    add marker editer and refactored code for map

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,31 +1,37 @@
 var initMap = require('./../js/map.js').initMap;
+var service = require('./../js/map.js').service;
 
 $( document ).ready(function() {
-  var selectType = '';
-  var selectKeyword = '';
-  initMap(selectType, selectKeyword);
+  //create a map
+  initMap();
 
   $("form.information").submit(function(event) {
     event.preventDefault();
+    var selectType = '';
+    var selectKeyword = '';
 
+    //set type and keyword
     newCatagory = $("select.catagory").val();
-    if (newCatagory === 'Coffee') {
-      selectType = 'cafe';
-      selectKeyword = 'coffee';
-    } else if (newCatagory === 'Restaurants') {
-      selectType = 'restaurant';
-      selectKeyword = '';
-    } else if (newCatagory === 'Parking') {
-      selectType = 'parking';
-      selectKeyword = '';
-    } else if (newCatagory === 'Fitness Centers') {
-      selectType = 'gym';
-      selectKeyword = '';
-    } else {
-      selectType = '';
-      selectKeyword = '';
-    }
+    if (newCatagory !== '') {
+      if (newCatagory === 'Coffee') {
+        selectType = 'cafe';
+        selectKeyword = 'coffee';
+      } else if (newCatagory === 'Restaurants') {
+        selectType = 'restaurant';
+        selectKeyword = '';
+      } else if (newCatagory === 'Parking') {
+        selectType = 'parking';
+        selectKeyword = '';
+      } else if (newCatagory === 'Fitness Centers') {
+        selectType = 'gym';
+        selectKeyword = '';
+      } else {
+        selectType = '';
+        selectKeyword = '';
+      }
 
-    initMap(selectType, selectKeyword);
+      //create new google.maps.places.PlacesService
+      service(selectType, selectKeyword);
+    }
   });
 });
--- js/map.js
+++ js/map.js
@@ -1,46 +1,70 @@
-var map;
+
 var infowindow;
+var markers =[];
+var pyrmont;
 
-exports.initMap = function (selectType, selectKeyword) {
+exports.initMap = function () {
+  //get current location
   navigator.geolocation.getCurrentPosition(function(location){
-    var pyrmont = {lat: location.coords.latitude, lng: location.coords.longitude};
+    pyrmont = {lat: location.coords.latitude, lng: location.coords.longitude};
 
+
+    //create map based on current location
     map = new google.maps.Map(document.getElementById('map'), {
       center: pyrmont,
       zoom: 15
     });
 
+    //set information for display on marker
     infowindow = new google.maps.InfoWindow();
-    var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch({
-      location: pyrmont,
-      radius: 500,
-      // type: ['cafe'],
-      // keyword: ['coffee']
-      type: [selectType],
-      keyword: [selectKeyword]
-    }, callback);
   });
+  return map;
 };
 
+//create new google.maps.places.PlacesService
+exports.service = function (selectType, selectKeyword) {
+  var newService = new google.maps.places.PlacesService(map);
+  newService.nearbySearch({
+    location: pyrmont,
+    radius: 500,
+    type: [selectType],
+    keyword: [selectKeyword]
+  }, callback);
+  return newService;
+};
+
+//returns markers and InfoWindow
 var callback = function (results, status) {
   if (status === google.maps.places.PlacesServiceStatus.OK) {
+    deleteMarkers();
     for (var i = 0; i < results.length; i++) {
-      createMarker(results[i]);
-    };
-  };
+      var marker = createMarker(results[i]);
+      markers.push(marker);
+    }
+  }
   console.log(results);
 };
 
 var createMarker = function (place) {
   var placeLoc = place.geometry.location;
+  //creates a Marker
   var marker = new google.maps.Marker({
     map: map,
     position: place.geometry.location
   });
 
+  //adds information to markers
   google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
+    infowindow.setContent(place.name + ' ' + place.vicinity);
     infowindow.open(map, this);
   });
+  return marker;
 };
+
+//deletes markers
+function deleteMarkers(){
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
+  }
+  markers = [];
+}
